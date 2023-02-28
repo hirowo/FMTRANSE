f0a969b1551ef804103caa9f1d5c4572ff48b848
hirowo <34668037+hirowo@users.noreply.github.com>
2023-02-28 22:53:41 +0900

    更新 main.ts

--- main.ts
+++ main.ts
@@ -63,7 +63,7 @@ namespace kagatranse {
     }
 
     //% blockId=radio_setfreq block="周波数%Freq |MHz"
-    //% Freq.min=60 Freq.max=90
+    //% Freq.min=60f Freq.max=90f
     export function Set_Freq(Freq: number) {
         let dsp = new QN8027;
         if (mode == 1) {
